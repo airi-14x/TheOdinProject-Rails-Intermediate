fbc279a92b8412914066906f1d1be791cb7b6ca2
airi-14x <airi.14x@gmail.com>
2019-12-29 12:58:18 -0500

    Test Warmup REST Get requests

--- rest-tester/app/controllers/users_controller.rb
+++ rest-tester/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    @name = "I am Index action"
+  end
+
+  def show
+    @name = "I am Show action"
+  end
+
+  def new
+    @name = "I am New action"
+  end
+
+  def edit
+    @name = "I am Edit action"
+  end
+
 end
--- rest-tester/app/script/rest_requestor.rb
+++ rest-tester/app/script/rest_requestor.rb
@@ -1,5 +1,15 @@
 require 'rest-client'
 url = "http://localhost:3000"
-url1 = "http://localhost:3000/users"
+index_url = "http://localhost:3000/users"
+show_url = "http://localhost:3000/users/1"
+new_url = "http://localhost:3000/users/new"
+edit_url = "http://localhost:3000/users/1/edit"
 
-puts RestClient.get(url1)
+create_url = "http://localhost:3000/users"
+
+#puts RestClient.get(index_url)
+#puts RestClient.get(show_url)
+#puts RestClient.get(new_url)
+puts RestClient.get(edit_url)
+
+#puts RestClient.post(create_url,"")
