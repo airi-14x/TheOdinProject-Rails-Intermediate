666c5ab312c7c5cda047f80c09f46a50d889abf5
airi-14x <airi.14x@gmail.com>
2020-05-12 12:42:58 -0400

    Refactor code to set User with attributes

--- re-former/app/controllers/application_controller.rb
+++ re-former/app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token #By passing CSRF error
+  skip_before_action :verify_authenticity_token #Bypass CSRF error (!)
 end
