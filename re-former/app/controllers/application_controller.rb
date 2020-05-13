332cc9ee1fae3035994f1b801df9961a7c2048f7
airi-14x <airi.14x@gmail.com>
2020-05-13 10:42:03 -0400

    Refactor with form_tag

--- re-former/app/controllers/application_controller.rb
+++ re-former/app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token #Bypass CSRF error (!)
+  #skip_before_action :verify_authenticity_token #Bypass CSRF error (!) --> HTML version
 end
