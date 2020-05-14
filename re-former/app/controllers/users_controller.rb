6c39b3e8286681f6e8bdc144187aa246f64d9bd0
airi-14x <airi.14x@gmail.com>
2020-05-14 11:11:23 -0400

    Add Edit and Update method

--- re-former/app/controllers/users_controller.rb
+++ re-former/app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
 
   def create
     # form_tag version
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    #@user = User.new(user: params[:user]) #Hash cannot be directly used
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(user: params[:user]) #Hash cannot be directly used
 
     # form_for version
     @user = User.new(user_params)
@@ -20,9 +21,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+
+  end
+
   private
+
   def user_params
     params.require(:user).permit(:username, :password, :email)
   end
-
 end
